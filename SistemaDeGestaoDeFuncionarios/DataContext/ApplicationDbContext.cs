bea5fdbbb364352bc4dd885a8f50dc9463785fc0
Gabriel <gabrielborgesschmidt@gmail.com>
2025-02-25 20:19:30 -0300

    first commit

---
+++ SistemaDeGestaoDeFuncionarios/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeGestaoDeFuncionarios.Models;
+
+namespace SistemaDeGestaoDeFuncionarios.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
---
+++ SistemaDeGestaoDeFuncionarios/Enums/TurnoEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SistemaDeGestaoDeFuncionarios.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TurnoEnum
+    {
+        Manha, 
+        Tarde,
+        Noite
+    }
+}
